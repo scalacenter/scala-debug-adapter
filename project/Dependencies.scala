890e33fa7f23acc6210adcec7a404b66e770f338
Scala Steward <me@scala-steward.org>
2021-06-15 18:50:57 +0200

    Update io to 1.5.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
   val javaDebug = "ch.epfl.scala" % "com-microsoft-java-debug-core" % "0.21.0+1-7f1080f1"
   val utest = "com.lihaoyi" %% "utest" % "0.6.6"
   val scalaCompiler = "org.scala-lang" % "scala-compiler" % scala212
-  val sbtIo = "org.scala-sbt" %% "io" % "1.4.0"
+  val sbtIo = "org.scala-sbt" %% "io" % "1.5.1"
   val sbtTestInterface = "org.scala-sbt" % "test-interface" % "1.0"
   val coursier = "io.get-coursier" %% "coursier" % "2.0.16"
 }
