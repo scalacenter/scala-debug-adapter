6937a8b137be1c2f852ba8358382792578f55f89
Scala Steward <me@scala-steward.org>
2021-06-16 10:07:13 +0200

    Update com-microsoft-java-debug-core to 0.32.0+1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
 
   val asm = "org.ow2.asm" % "asm" % asmVersion
   val asmUtil = "org.ow2.asm" % "asm-util" % asmVersion
-  val javaDebug = "ch.epfl.scala" % "com-microsoft-java-debug-core" % "0.21.0+1-7f1080f1"
+  val javaDebug = "ch.epfl.scala" % "com-microsoft-java-debug-core" % "0.32.0+1"
   val utest = "com.lihaoyi" %% "utest" % "0.7.10"
   val scalaCompiler = "org.scala-lang" % "scala-compiler" % scala212
   val sbtIo = "org.scala-sbt" %% "io" % "1.5.1"
