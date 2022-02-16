157a120f1c9ab70ecaa1fe2b7830a88fbcda6cfc
Adrien Piquerez <adrien.piquerez@gmail.com>
2022-02-16 16:09:22 +0100

    Deactivate test on Java 17

--- sbt/plugin/src/sbt-test/debug-session/native-lib/src/main/scala/example/NativeCall.scala
+++ sbt/plugin/src/sbt-test/debug-session/native-lib/src/main/scala/example/NativeCall.scala
@@ -2,10 +2,16 @@ package example
 
 import java.nio.file.Files
 import scala.util.control.NonFatal
+import scala.util.Properties
 
 object NativeCall extends App {
-  val env = org.lmdbjava.Env.create()
-  val folder = Files.createTempDirectory("test").toFile
-  env.open(folder)
+  if (Properties.javaVersion.startsWith("17")) {
+    // There is no support for lmdbjava in Java 17
+    ()
+  } else {
+    val env = org.lmdbjava.Env.create()
+    val folder = Files.createTempDirectory("test").toFile
+    env.open(folder)
+  }
   println("Success")
 }
