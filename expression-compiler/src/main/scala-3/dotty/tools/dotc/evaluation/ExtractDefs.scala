37c1ac68cc9d144cbd64db92d13723edc2e19ffc
Adrien Piquerez <adrien.piquerez@gmail.com>
2022-05-20 13:23:35 +0200

    Add test for evaluation inside nested methods

--- expression-compiler/src/main/scala-3/dotty/tools/dotc/evaluation/ExtractDefs.scala
+++ expression-compiler/src/main/scala-3/dotty/tools/dotc/evaluation/ExtractDefs.scala
@@ -1,12 +1,12 @@
 package dotty.tools.dotc.evaluation
 
 import dotty.tools.dotc.EvaluationContext
-import dotty.tools.dotc.ast.tpd._
-import dotty.tools.dotc.core.Contexts._
-import dotty.tools.dotc.core.Symbols.ClassSymbol
+import dotty.tools.dotc.ast.tpd.*
+import dotty.tools.dotc.core.Contexts.*
+import dotty.tools.dotc.core.Symbols.*
 import dotty.tools.dotc.transform.MegaPhase.MiniPhase
 import dotty.tools.dotc.transform.SymUtils.isField
-import dotty.tools.dotc.core.Flags
+import dotty.tools.dotc.core.Flags.*
 
 /**
  * This phase is responsible for extracting types of definitions (`DefDef` and `ValDef`) and anonymous functions.
@@ -22,7 +22,7 @@ class ExtractDefs(using evalCtx: EvaluationContext) extends MiniPhase:
 
   override def transformDefDef(tree: DefDef)(using Context): Tree =
     // extract nested methods
-    if isCorrectOwner(tree) && tree.symbol.owner.is(Flags.Method) then
+    if isCorrectOwner(tree) && tree.symbol.owner.is(Method) then
       evalCtx.nestedMethods += tree.symbol.denot -> tree
     super.transformDefDef(tree)
 
