2ba495f531b5f4a681f4744ce353392222946b01
Adrien Piquerez <adrien.piquerez@gmail.com>
2022-10-03 13:17:24 +0200

    Fix step into lazy init

--- core/src/main/scala/ch/epfl/scala/debugadapter/internal/stepfilter/RuntimeStepFilter.scala
+++ core/src/main/scala/ch/epfl/scala/debugadapter/internal/stepfilter/RuntimeStepFilter.scala
@@ -8,6 +8,9 @@ private class RuntimeStepFilter(
     classesToSkip: Set[String],
     methodsToSkip: Set[String]
 ) extends StepFilter {
+  override def shouldStepInto(method: Method): Boolean =
+    classesToSkip.contains(method.declaringType.name) ||
+      methodsToSkip.contains(method.toString)
   override def shouldStepOut(upperLocation: Location, method: Method): Boolean =
     classesToSkip.contains(method.declaringType.name) ||
       methodsToSkip.contains(method.toString)
