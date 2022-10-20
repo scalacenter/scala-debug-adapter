2eeed25d285107457fc47520323097edccd48345
Adrien Piquerez <adrien.piquerez@gmail.com>
2022-10-20 17:32:00 +0200

    Fix Scala 2.12.10 compilation

--- expression-compiler/src/main/scala-2/scala/tools/nsc/ExpressionCompilerBridge.scala
+++ expression-compiler/src/main/scala-2/scala/tools/nsc/ExpressionCompilerBridge.scala
@@ -6,7 +6,6 @@ import java.{util => ju}
 import scala.jdk.CollectionConverters._
 import scala.tools.nsc.reporters.StoreReporter
 import scala.util.control.NonFatal
-import scala.annotation
 
 final class ExpressionCompilerBridge {
   def run(
@@ -32,7 +31,7 @@ final class ExpressionCompilerBridge {
     ) ++ options :+ sourceFile.toString
 
     val command = new CompilerCommand(args, errorConsumer.accept(_))
-    val reporter = new StoreReporter(): @annotation.nowarn()
+    val reporter = new StoreReporter()
     val global = new ExpressionGlobal(
       command.settings,
       reporter,
