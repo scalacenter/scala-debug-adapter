6e890459fa961d9007e00dcbd9932ef1676b4862
Adrien Piquerez <adrien.piquerez@gmail.com>
2024-01-04 14:53:31 +0100

    Update scala versions

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,9 +3,9 @@ import sbt._
 object Dependencies {
   val scalaEnvVersion = Option(System.getenv("SCALA_VERSION"))
   val scala212 = scalaEnvVersion.filter(isScala212).getOrElse("2.12.18")
-  val scala213 = scalaEnvVersion.filter(isScala213).getOrElse("2.13.11")
+  val scala213 = scalaEnvVersion.filter(isScala213).getOrElse("2.13.12")
   val scala30 = scalaEnvVersion.filter(isScala30).getOrElse("3.0.2")
-  val scala31Plus = scalaEnvVersion.filter(isScala31Plus).getOrElse("3.3.0")
+  val scala31Plus = scalaEnvVersion.filter(isScala31Plus).getOrElse("3.3.1")
   val asmVersion = "9.6"
   val coursierVersion = "2.1.8"
 
