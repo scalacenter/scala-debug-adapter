eb04a8a4e55579c09461f892b2cee7b1646a72a2
Lucas Nouguier <lucas.nouguier@protonmail.com>
2024-02-26 16:43:33 +0100

    class loading filter name fix

--- modules/core/src/main/scala/ch/epfl/scala/debugadapter/internal/package.scala
+++ modules/core/src/main/scala/ch/epfl/scala/debugadapter/internal/package.scala
@@ -3,7 +3,7 @@ package ch.epfl.scala.debugadapter
 package object internal {
   val runtimeFilterName = "runtime"
   val decoderFilterName = "decoder"
-  val classLoadingFilterName = "classLoader"
+  val classLoadingFilterName = "classLoading"
   val defaultFiltersName = Array(runtimeFilterName, classLoadingFilterName, decoderFilterName)
   val defaultFilters = defaultFiltersName.map(_ -> true).toMap
 }
