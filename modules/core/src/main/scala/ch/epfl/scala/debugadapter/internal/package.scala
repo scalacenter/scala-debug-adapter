e28592afa5b3f1574f0385cf65304f5246730a78
Lucas Nouguier <lucas.nouguier@protonmail.com>
2024-02-28 10:47:03 +0100

    removed unused

--- modules/core/src/main/scala/ch/epfl/scala/debugadapter/internal/package.scala
+++
@@ -1,9 +0,0 @@
-package ch.epfl.scala.debugadapter
-
-package object internal {
-  val runtimeFilterName = "runtime"
-  val decoderFilterName = "decoder"
-  val classLoadingFilterName = "classLoading"
-  val defaultFiltersName = Array(runtimeFilterName, classLoadingFilterName, decoderFilterName)
-  val defaultFilters = defaultFiltersName.map(_ -> true).toMap
-}
--- modules/tests/src/test/scala/ch/epfl/scala/debugadapter/StepFilterTests.scala
+++ modules/tests/src/test/scala/ch/epfl/scala/debugadapter/StepFilterTests.scala
@@ -198,31 +198,6 @@ class Scala213StepFilterTests extends StepFilterTests(ScalaVersion.`2.13`) {
       StepIn.method("ScalaRunTime$.wrapRefArray(Object[]): ArraySeq")
     )
   }
-
-  test("should match all kinds of Scala 2 types (not valid in Scala 3)") {
-    val source =
-      """|package example
-         |
-         |trait A {
-         |  class B
-         |}
-         |
-         |object Main extends A {
-         |  class B
-         |  def m(b: Main.super[A].B): Main.super[A].B = b
-         |  def m(x: Either[Int, X] forSome { type X }): Either[Y, Int] forSome { type Y } = x.swap
-         |
-         |  def main(args: Array[String]): Unit = {
-         |    val b0: super[A].B = new super[A].B
-         |    m(b0)
-         |    val x = Right(2)
-         |    m(x)
-         |  }
-         |}
-         |""".stripMargin
-    implicit val debuggee: TestingDebuggee = TestingDebuggee.mainClass(source, "example.Main", ScalaVersion.`2.13`)
-    check(Breakpoint(14), StepIn.line(9), Breakpoint(16), StepIn.line(10))
-  }
 }
 
 abstract class StepFilterTests(protected val scalaVersion: ScalaVersion) extends DebugTestSuite {
