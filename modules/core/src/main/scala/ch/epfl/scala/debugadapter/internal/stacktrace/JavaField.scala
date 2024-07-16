da5e36b0da7ce7215c4815dfab724327a5f2f606
Saad Aissaoui <saad.aissaoui@epfl.ch>
2024-07-16 13:38:42 +0200

    decode variables and fields basics

--- modules/core/src/main/scala/ch/epfl/scala/debugadapter/internal/stacktrace/JavaField.scala
+++ modules/core/src/main/scala/ch/epfl/scala/debugadapter/internal/stacktrace/JavaField.scala
@@ -4,6 +4,11 @@ import com.sun.jdi
 import com.microsoft.java.debug.core.adapter.stacktrace.DecodedField
 
 final case class JavaField(field: jdi.Field) extends DecodedField {
+
+  override def show(): Boolean = 
+    field.isStatic() || true // TODO // should be valdef in object
+
+    
   def format: String = {
     field.name()
   }
