d5cad4b1fc10277b6e6157d3ce5c0844bf16f011
Saad Aissaoui <saad.aissaoui@epfl.ch>
2024-07-18 16:46:31 +0200

    fix for pr

--- modules/core/src/main/scala/ch/epfl/scala/debugadapter/internal/stacktrace/JavaField.scala
+++ modules/core/src/main/scala/ch/epfl/scala/debugadapter/internal/stacktrace/JavaField.scala
@@ -5,10 +5,9 @@ import com.microsoft.java.debug.core.adapter.stacktrace.DecodedField
 
 final case class JavaField(field: jdi.Field) extends DecodedField {
 
-  override def show(): Boolean = 
+  override def show(): Boolean =
     field.isStatic() || true // TODO // should be valdef in object
 
-    
   def format: String = {
     field.name()
   }
