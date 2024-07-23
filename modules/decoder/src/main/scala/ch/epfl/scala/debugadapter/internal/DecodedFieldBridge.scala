9fbf899221ce11969d5782048f652132d62f6601
Saad Aissaoui <saad.aissaoui@epfl.ch>
2024-07-23 18:07:23 +0200

    fields format

--- modules/decoder/src/main/scala/ch/epfl/scala/debugadapter/internal/DecodedFieldBridge.scala
+++ modules/decoder/src/main/scala/ch/epfl/scala/debugadapter/internal/DecodedFieldBridge.scala
@@ -4,6 +4,7 @@ import ch.epfl.scala.decoder.DecodedField
 import ch.epfl.scala.decoder.StackTraceFormatter
 import tastyquery.Symbols.*
 import tastyquery.Modifiers.*
+import tastyquery.Names.*
 import ch.epfl.scala.decoder.DecodedField.ValDef
 import ch.epfl.scala.decoder.DecodedField.ModuleVal
 import ch.epfl.scala.decoder.DecodedField.LazyValOffset
@@ -13,7 +14,7 @@ import ch.epfl.scala.decoder.DecodedField.LazyValBitmap
 
 class DecodedFieldBridge(field: DecodedField):
   def format: String = field match
-    case field: ValDef => field.symbol.name.toString()
+    case field: ValDef => format(field.symbol.name)
     case field: ModuleVal => ""
     case field: LazyValOffset => ""
     case field: Outer => "<outer>"
@@ -24,3 +25,13 @@ class DecodedFieldBridge(field: DecodedField):
   def show: Boolean = true
 
   // formatter.format(variable)
+  private def format(name: Name): String =
+    def rec(name: Name): String = name match
+      case DefaultGetterName(termName, num) => s"${termName.toString()}.<default ${num + 1}>"
+      case name: TypeName => rec(name.toTermName)
+      case SimpleName("$anonfun") => "<anon fun>"
+      case SimpleName("$anon") => "<anon class>"
+      case ObjectClassName(underlying) => rec(underlying)
+      case UniqueName(SimpleName(""), _, _) => "<anon>"
+      case _ => name.toString
+    rec(name)
