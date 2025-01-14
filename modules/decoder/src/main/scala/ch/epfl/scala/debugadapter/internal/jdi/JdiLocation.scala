9d5cea407a4e4536d8432cfd5f5a9e0093450b72
Adrien Piquerez <adrien.piquerez@gmail.com>
2025-01-14 14:58:26 +0100

    Remove duplicated classes

--- modules/decoder/src/main/scala/ch/epfl/scala/debugadapter/internal/jdi/JdiLocation.scala
+++
@@ -1,4 +0,0 @@
-package ch.epfl.scala.debugadapter.internal.jdi
-
-class JdiLocation(val obj: Any) extends JavaReflection(obj, "com.sun.jdi.Location"):
-  def lineNumber: Int = invokeMethod[Int]("lineNumber")
